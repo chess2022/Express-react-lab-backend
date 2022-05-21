41e84d38a7411877e49a7d947d2c2c53b4f49a08
chess2022 <lakeshore.hnh@gmail.com>
2022-05-21 11:24:08 -0700

    backend is done

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import dependencies
+const express = require("express")
+const cors = require("cors");
+const PORT = process.send.PORT || 4000
+
+// Import json files
+const projects = require("./projects.json")
+const about = require("./about.json")
+
+// Set up middleware
+
+const app = express();
+
+// ROUTES 
+// home for testing
+app.get("/", (req, res) => {
+    res.send("Testing, testing, one, two, three")
+})
+
+// route for projects
+app.get("/projects", (req, res) => {
+    res.json(projects)
+})
+
+// route for about 
+app.get("/about", (req, res) => {
+    res.json(about)
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
